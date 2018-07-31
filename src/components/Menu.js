46e049681478d2c80ce2e83851296d8f0548da58
ericzakariasson <eric.zakariasson@gmail.com>
2018-07-31 15:14:18 +0200

    Updated Menu appearance and css

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,21 +3,24 @@ import styled from 'styled-components';
 import { NavLink, withRouter } from 'react-router-dom';
 import ScrollLock from 'react-scrolllock';
 import { Hash, MapPin, Settings } from 'react-feather';
-import { Keyframes, animated, config } from 'react-spring';
+import { Transition, Keyframes, animated, config } from 'react-spring';
 
 
 import Hamburger from './Hamburger';
 import Logo from './Logo';
 
 
+const HEADER_HEIGHT = 50;
+
 const StyledHeader = styled.header`
   width: 100%;
-  height: 50px;
+  height: ${HEADER_HEIGHT}px;
   padding: 0 20px;
   background: #d7c89433;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  position: relative;
 `;
 
 const Wrapper = styled.div`
@@ -26,24 +29,31 @@ const Wrapper = styled.div`
 
 const MenuWrapper = styled(animated.ul)`
   position: absolute;
-  top: 50px;
+  top: ${HEADER_HEIGHT}px;
   left: 0;
-  height: calc(100vh - 50px);
   width: 100%;
-  background: rgba(0,0,0,0.08);
   overflow: hidden;
 `;
 
 const MenuItem = styled(animated.li)`
   width: 100%;
   padding: 30px 20px;
-  border-bottom: 1px solid #EEE;
   background: #FFF;
   overflow: hidden;
+  transition: ${p => p.theme.transition} border;
+  border-bottom-color: #EEE;
+
+  &:not(:last-child) {
+  border-bottom: ${p => p.open ? '1px solid #EEE' : 'none'};
+  }
+
+  &:last-child {
+    border-radius: 0 0 5px 5px;
+  }
 `;
 
 const StyledNavLink = styled(NavLink)`
-  font-size: 3.6rem;
+  font-size: 2.4rem;
   font-weight: 500;
   color: #444;
   text-decoration: none;
@@ -64,6 +74,16 @@ const StyledNavLink = styled(NavLink)`
   }
 `;
 
+const MenuBackground = styled(animated.div)`
+  background: rgba(0,0,0,0.08);
+  position: absolute;
+  top: ${HEADER_HEIGHT}px;
+  left: 0;
+  z-index: -1;
+  height: calc(100vh - ${HEADER_HEIGHT}px);
+  width: 100%;
+`;
+
 const routes = [
   {
     path: '/platser',
@@ -82,18 +102,13 @@ const routes = [
   },
 ]
 
-const MenuBackground = Keyframes.Spring({
-  open: { to: { height: (window.innerHeight - 50) } },
-  close: { delay: 100, to: { height: 0 } }
-})
-
 const MenuContent = Keyframes.Trail({
   open: { delay: 200, to: { opacity: 1, height: 'auto', padding: 30 }, config: config.stiff },
   close: { to: { opacity: 1, height: 0, padding: 0 }, config: config.stiff }
 })
 
 class Menu extends Component {
-  state = { isOpen: true }
+  state = { isOpen: false }
 
   componentDidUpdate(prevProps) {
     const { location: { pathname } } = this.props;
@@ -108,31 +123,36 @@ class Menu extends Component {
 
   render() {
 
-    const state = this.state.isOpen ? 'open' : 'close';
+    const { isOpen } = this.state;
+    const state = isOpen ? 'open' : 'close';
 
     return (
       <Wrapper>
+        {isOpen && <ScrollLock />}
         <StyledHeader>
           <Logo />
-          <Hamburger onClick={this.toggleOpen} isOpen={this.state.isOpen} />
+          <Hamburger onClick={this.toggleOpen} isOpen={isOpen} />
         </StyledHeader>
-        <MenuBackground native state={state}>
-          {({ height }) => (
-            <MenuWrapper style={{ height }}>
-              <MenuContent keys={routes.map((_, i) => i)} native state={state}>
-                {routes.map(route => styles => (
-                  <MenuItem style={{ ...styles, padding: styles.padding.interpolate(p => `${p}px 20px`) }}>
-                    <StyledNavLink to={route.path}>
-                      <route.Icon color="#222" style={{ marginRight: '20px' }} />
-                      {route.label}
-                    </StyledNavLink>
-                  </MenuItem>
-                ))}
-              </MenuContent>
-            </MenuWrapper>
-          )}
-        </MenuBackground>
-        <ScrollLock />
+        <MenuWrapper>
+          <MenuContent keys={routes.map((_, i) => _.path)} native state={state}>
+            {routes.map(route => styles => (
+              <MenuItem open={isOpen} style={{ ...styles, padding: styles.padding.interpolate(p => `${p}px 20px`) }}>
+                <StyledNavLink to={route.path}>
+                  <route.Icon color="#222" style={{ marginRight: '20px' }} />
+                  {route.label}
+                </StyledNavLink>
+              </MenuItem>
+            ))}
+          </MenuContent>
+        </MenuWrapper>
+        <Transition
+          native
+          from={{ opacity: 0 }}
+          enter={{ opacity: 1 }}
+          leave={{ opacity: 0 }}
+        >
+          {isOpen && (style => <MenuBackground style={style} />)}
+        </Transition>
       </Wrapper>
     )
   }
