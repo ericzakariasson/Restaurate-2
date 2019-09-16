f7605a5776b84f1bca35c21232307c4083451ac6
Eric <eric.zakariasson@gmail.com>
2019-09-16 21:01:22 +0200

    Use env variable for api url

--- packages/web/src/apollo.ts
+++ packages/web/src/apollo.ts
@@ -2,7 +2,10 @@ import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-const uri = `/graphql`;
+const uri =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_API_URL
+    : `/graphql`;
 
 export const client = new ApolloClient({
   link: new HttpLink({ uri, credentials: 'include' }),
