f94451cf9f45b632225b5d77530097f5eb32440a
Eric <eric.zakariasson@gmail.com>
2019-09-17 19:44:29 +0200

    Add netlify redirects

--- packages/web/src/apollo.ts
+++ packages/web/src/apollo.ts
@@ -2,14 +2,9 @@ import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-const uri =
-  process.env.NODE_ENV === 'production'
-    ? process.env.REACT_APP_API_URL
-    : '/graphql';
-
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri,
+    uri: process.env.REACT_APP_API_URL,
     credentials: 'include'
   }),
   cache: new InMemoryCache()
