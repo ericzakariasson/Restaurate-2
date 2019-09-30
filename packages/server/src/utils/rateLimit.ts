c609ec248c82345e36d63cc222877d013b3edda9
ericzakariasson <eric.zakariasson@gmail.com>
2019-09-30 18:14:31 +0200

    Disable rate limit in dev

--- packages/server/src/utils/rateLimit.ts
+++ packages/server/src/utils/rateLimit.ts
@@ -2,6 +2,7 @@ import { MiddlewareFn } from 'type-graphql';
 import { redis } from '../services/redis/redis';
 import { Context } from '../graphql/types';
 import { days } from './constants';
+import { isProduction } from './env.helper';
 
 const ONE_DAY = days(1);
 
@@ -11,9 +12,13 @@ export const rateLimitAuthenticated: (
   { context: { req }, info },
   next
 ) => {
-  const key = `rate-limit:${info.fieldName}:${req.session.userId!}`;
+  if (!isProduction()) {
+    return next();
+  }
 
+  const key = `rate-limit:${info.fieldName}:${req.session.userId!}`;
   const current = await redis.incr(key);
+
   if (current > limit) {
     throw new Error("You're doing that too much");
   } else if (current === 1) {
@@ -26,9 +31,13 @@ export const rateLimitAuthenticated: (
 export const rateLimit: (limit?: number) => MiddlewareFn<Context> = (
   limit = 50
 ) => async ({ context: { req }, info }, next) => {
-  const key = `rate-limit:${info.fieldName}:${req.ip}`;
+  if (!isProduction()) {
+    return next();
+  }
 
+  const key = `rate-limit:${info.fieldName}:${req.ip}`;
   const current = await redis.incr(key);
+
   if (current > limit) {
     throw new Error("You're doing that too much");
   } else if (current === 1) {
