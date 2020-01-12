649abeb93764e1b568f8e56e394a2d07ba6ffafb
--global <eric.zakariasson@gmail.com>
2020-01-12 14:22:26 +0100

    Implement tag data loader

--- packages/server/src/modules/place/tag/tag.repository.ts
+++ packages/server/src/modules/place/tag/tag.repository.ts
@@ -1,29 +1,33 @@
-import { Repository } from 'typeorm';
+import { Repository, EntityRepository } from 'typeorm';
 import { Tag } from './tag.entity';
+import * as DataLoder from 'dataloader';
 
+@EntityRepository(Tag)
 export class TagRepository extends Repository<Tag> {
-  findByPlaceId(placeId: number) {
-    return this.createQueryBuilder('tag')
-      .innerJoin('tag.place', 'place', 'place.id = :placeId', {
-        placeId
-      })
-      .select('tag.*')
-      .getRawMany();
-  }
+  private loader: DataLoder<number, Tag[]> = new DataLoder(async placeIds => {
+    const tags = await this.findByPlaceIds(placeIds);
 
-  findByPlaceIds(placeIds: number[]) {
-    return this.createQueryBuilder('tag')
+    const mapped = placeIds.map(placeId =>
+      tags.filter(tag => tag.place_id === placeId)
+    );
+
+    return mapped;
+  });
+
+  findByPlaceId = (placeId: number) => this.loader.load(placeId);
+
+  findByPlaceIds = (placeIds: readonly number[]) =>
+    this.createQueryBuilder('tag')
       .innerJoin('tag.place', 'place', 'place.id IN (:...placeIds)', {
         placeIds
       })
       .select('tag.*')
+      .addSelect('place.id')
       .getRawMany();
-  }
 
-  findByUserId(userId: number) {
-    return this.createQueryBuilder('tag')
+  findByUserId = (userId: number) =>
+    this.createQueryBuilder('tag')
       .innerJoin('tag.user', 'user', 'user.id = :userId', { userId })
       .select('tag.*')
       .getRawMany();
-  }
 }
